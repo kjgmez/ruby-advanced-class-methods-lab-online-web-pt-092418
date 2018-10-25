a3ca32d28da16388b5534b8ce1d1eebdaed2c539
Kevin J Gomez <kjgmez1@gmail.com>
2018-10-25 22:38:12 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -8,6 +9,44 @@ class Song
 
   def save
     self.class.all << self
+  end
+
+  def self.create
+    song = self.new
+    song.save
+    song
+  end
+
+  def self.new_by_name(sname)
+    song = self.new # creates a new song
+    song.name = sname
+    song
+  end
+
+  def self.create_by_name(sname)
+    song = self.new
+    song.name = sname
+    song.save
+    song
+  end
+
+  def self.find_by_name(sname)
+    self.all.find{|song| song.name == sname}
+  end
+
+  def self.find_or_create_by_name(sname)
+    self.find_by_name(sname) || self.create_by_name(sname)
+  end
+
+  def self.alphabetical
+    self.all.sort_by{|x| x.name}
+  end
+
+  def self.new_from_filename(fname)
+    file = self.new
+    file.name = fname.split("-", 3)
+    x
+    puts "la"
   end
 
 end
